3c9a3e8c6060f63417f2cf9b1656fb387d607623
Shreemathi29 <shreemathiramesh29@gmail.com>
2022-11-08 12:23:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory = "templates")
+
+@app.get('/')
+def home():
+    
+    return "Welcome"
+
+@app.get("/greet")
+def greet_user(name):
+   
+    
+    name = input("Enter your Username: ")
+    password = int(input("Enter your Password: "))
+    if( password == 923104):
+        
+        
+        return " Login Successfully: " +name
+    else:
+        return "Failed"
